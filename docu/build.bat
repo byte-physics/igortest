ec01433e62b631dc1578edac7b10eedb645e9d82
Thomas Braun <thomas.braun@byte-physics.de>
2013-02-26 22:17:37 +0100

    move documentation into its own folder add copyright header to mainpage.dox tweak pdf index with makeindex style file

--- docu/build.bat
+++ docu/build.bat
@@ -1,3 +1,3 @@
-h:\projekte\physical-value\libs\doxygen-1.8.1.1-win32\doxygen.exe
-cd latex && make.bat && cd ..
-cp latex/refman.pdf .
+doxygen
+cd latex && call make.bat && cd ..
+copy latex\refman.pdf .
