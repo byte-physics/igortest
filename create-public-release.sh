a4a5855fb4dc459a70e6b9545ee9f6193032cdad
Thomas Braun <thomas.braun@byte-physics.de>
2013-02-27 12:05:16 +0100

    adapt release script

--- create-public-release.sh
+++ create-public-release.sh
@@ -12,20 +12,27 @@ if [ ! -z "$(git status -s --untracked-files=no $filesToWatch)" ]; then
 	exit 0
 fi
 
-baseName=unitTestingFramework-v$newVersion
-zipFile=$baseName.zip
-folder=public-releases/$baseName
+basename=UnitTestingFramework-v$newVersion
+zipFile=$basename.zip
+folder=releases/$basename
 
 rm -rf $folder
 rm -rf $zipfile
 
 mkdir -p $folder
 
-cp -r procedures docu/examples INSTALL.txt helper docu/refman.pdf $folder
+cp -r procedures docu/examples INSTALL.txt helper $folder
+
+# copy and rename manual
+cp docu/refman.pdf $folder/Manual-$basename.pdf
+
+# copy autorun script into example6 folder
+cp $folder/helper/autorun-test.bat $folder/examples/Example6
 
 git rev-parse $revision > internalVersion
 
-cd public-releases &&  zip -z -q -r $baseName.zip $baseName/* < ../internalVersion && cd ..
+cd releases && zip -m -z -q -r $basename.zip $basename/* < ../internalVersion && cd ..
 
+rmdir $folder
 rm internalVersion
 
