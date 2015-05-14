411a91ff3218adc870cee4d962ed296fa6498b36
Thomas Braun <thomas.braun@byte-physics.de>
2015-05-14 15:40:41 +0200

    Call pdflatex in non stop mode

--- docu/build.bat
+++ docu/build.bat
@@ -1,8 +1,8 @@
 doxygen
 cd latex
-pdflatex -shell-escape refman.tex
+pdflatex -interaction=nonstopmode -shell-escape refman.tex
 makeindex -s ../refman.ist refman.idx
-pdflatex -shell-escape refman.tex
-pdflatex -shell-escape refman.tex
+pdflatex -interaction=nonstopmode -shell-escape refman.tex
+pdflatex -interaction=nonstopmode -shell-escape refman.tex
 cd ..
 copy latex\refman.pdf .
