64446bceee99abe9cce1b6288bd6e3cd031bd54f
Matthias Kastner <matthias.kastner@byte-physics.de>
2018-04-07 18:34:05 +0200

    fix pygments
    sometimes the pygments directory is leading to strange output errors.
    Deleting the directory works around the problem.

--- docu/build.sh
+++ docu/build.sh
@@ -7,6 +7,7 @@ cd "$DOCUMENTATION_ROOT"
 
 doxygen
 cd latex
+rm -Rf _minted-*
 pdflatex -interaction=nonstopmode -shell-escape refman.tex
 makeindex -s ../refman.ist refman.idx
 pdflatex -interaction=nonstopmode -shell-escape refman.tex
