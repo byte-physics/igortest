0d34a829c61adbf8ef2dce4cf9c5cf03aae2a156
Matthias Kastner <matthias.kastner@byte-physics.de>
2018-07-11 14:35:59 +0200

    remove batch file
    Now that we have docker containers, windows compatibility is given
    by using those containers.
    Although, it is not yet tested on Windows 10, it is expected to work.

--- docu/build.bat
+++
@@ -1,8 +0,0 @@
-doxygen
-cd latex
-pdflatex -interaction=nonstopmode -shell-escape refman.tex
-makeindex -s ../refman.ist refman.idx
-pdflatex -interaction=nonstopmode -shell-escape refman.tex
-pdflatex -interaction=nonstopmode -shell-escape refman.tex
-cd ..
-copy latex\refman.pdf .
