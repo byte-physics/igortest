2bf4653bef20f81d2ee5bd5a16e7d01ed3ac1102
Thomas Braun <thomas.braun@byte-physics.de>
2018-10-10 20:28:47 +0200

    Documentation: Adapt matomo tracking code

--- docu/sphinx/source/_static/js/matomo.js
+++ docu/sphinx/source/_static/js/matomo.js
@@ -8,6 +8,8 @@
     var u="https://www.byte-physics.de/piwik/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '1']);
+    _paq.push(['setCookieDomain', '*.byte-physics.de']);
+    _paq.push(['setDomains', '*.byte-physics.de']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
