byte-physics/igortest

commit 06b2e062e6f8cf5890e484a09b992e5091c7a364

WaveType extension
WaveType(wv, 1) and WaveType(wv, 2) allow to test for NULL_WAVE
In addition they give access to a broader range of wave type
definitions.

WaveType(wv, 0) requires an existing wave to yield valuable output.
The check was included in the optional function

procedures/unit-testing-comparators.ipf
@@ -469,29 +469,28 @@ static Function TEST_WAVE_WRAPPER(wv, majorType, flags, [minorType])
 	variable majorType, minorType
 	variable flags
 
+	variable result, type, type1, type2
+	string str
+
 	incrAssert()
 
 	if(shouldDoAbort())
 		return NaN
 	endif
 
-	variable result = WaveExists(wv)
-	DebugOutput("Assumption that the wave exists", result)
-
-	if(!result)
-		if(flags & OUTPUT_MESSAGE)
-			printFailInfo()
-		endif
-		if(flags & INCREASE_ERROR)
-			incrError()
-		endif
-		if(flags & ABORT_FUNCTION)
-			abortNow()
-		endif
+	type2 = WaveType(wv, 2)
+	type1 = WaveType(wv, 1)
+	if(type1 == NULL_WAVE && type2 == NULL_WAVE)
+		type = NULL_WAVE
 	endif
+	if(type1 > 0 && type1 <= 4)
+		type = type | 2^(type1 - 1)
+	endif
+	if(type2 > 0 && type2 <= 2)
+		type = type | 2^(type2 + 3)
+	endif
+	result = type & majorType
 
-	result = (WaveType(wv, 1) == majorType)
-	string str
 	sprintf str, "Assumption that the wave's main type is %d", majorType
 	DebugOutput(str, result)
 
@@ -508,8 +507,9 @@ static Function TEST_WAVE_WRAPPER(wv, majorType, flags, [minorType])
 	endif
 
 	if(!ParamIsDefault(minorType))
-		result = WaveType(wv, 0) & minorType
+		type = WaveExists(wv) ? WaveType(wv, 0) : NULL_WAVE
 
+		result = type & minorType
 		sprintf str, "Assumption that the wave's sub type is %d", minorType
 		DebugOutput(str, result)
 

internal_dev/TestWaveType.ipf
@@ -1,0 +1,81 @@
+#pragma TextEncoding = "UTF-8"
+#pragma rtGlobals=3		// Use modern global access method and strict wave access.
+
+#include "unit-testing"
+
+Function run_IGNORE()
+	RunTest("TestWaveType.ipf", name="Test different Wave Types", testCase="testWaveTypes")
+End
+
+Function testWaveTypes()
+	WAVE/Z wv
+	CHECK_WAVE(wv, NULL_WAVE)
+
+	Make/FREE/U/I wv0
+	CHECK_WAVE(wv0, FREE_WAVE, minorType = UNSIGNED_WAVE)
+	CHECK_WAVE(wv0, NUMERIC_WAVE, minorType = INT32_WAVE)
+
+	Make/FREE/T wv1
+	CHECK_WAVE(wv1, FREE_WAVE)
+	CHECK_WAVE(wv1, TEXT_WAVE)
+
+	Make/O/U/I root:wv2/WAVE=wv2
+	CHECK_WAVE(wv2, NORMAL_WAVE, minorType = UNSIGNED_WAVE)
+	CHECK_WAVE(wv2, NUMERIC_WAVE, minorType = INT32_WAVE)
+End
+
+Function checkWaveType()
+	WAVE/Z wv
+	printWaveType(wv)
+
+	Make/FREE/U/I wv0
+	printWaveType(wv0)
+
+	Make/FREE/T wv1
+	printWaveType(wv1)
+
+	Make/O/U/I root:wv2/WAVE=wv2
+	printWaveType(wv2)
+End
+
+Function printWaveType(wv)
+	WAVE/Z wv
+
+	Variable type2 = WaveType(wv, 2)
+	Variable type1 = WaveType(wv, 1)
+	Variable type0 = WaveExists(wv) ? WaveType(wv, 0) : NULL_WAVE
+
+	Variable myType = type0
+	myType = myType | (type1 == NULL_WAVE ? NULL_WAVE : 2^(type1 + 7))
+	myType = myType | (type2 == NULL_WAVE ? NULL_WAVE : 2^(type2 + 11))
+
+	print "current Wave Type"
+	printf "%016b\r", myType
+
+	print "Wave Type Definitions"
+	printf "%016b\r", myType & NULL_WAVE
+
+	print "WaveType(wv, 0)"
+	printf "%016b  compl\r", myType & COMPLEX_WAVE
+	printf "%016b  float\r", myType & FLOAT_WAVE
+	printf "%016b double\r", myType & DOUBLE_WAVE
+	printf "%016b   int8\r", myType & INT8_WAVE
+	printf "%016b  int16\r", myType & INT16_WAVE
+	printf "%016b  int32\r", myType & INT32_WAVE
+	printf "%016b  int64\r", myType & INT64_WAVE
+	printf "%016b   uint\r", myType & UNSIGNED_WAVE
+
+	print "WaveType(wv, 1)"
+	printf "%016b    num\r", myType & NUMERIC_WAVE
+	printf "%016b   text\r", myType & TEXT_WAVE
+	printf "%016b    dfr\r", myType & DATAFOLDER_WAVE
+	printf "%016b   wave\r", myType & WAVE_WAVE
+
+	print "WaveType(wv, 2)"
+	printf "%016b normal\r", myType & NORMAL_WAVE
+	printf "%016b   free\r", myType & FREE_WAVE
+
+	print myType & NUMERIC_WAVE && myType & FREE_WAVE, "free and numeric"
+	print myType & NUMERIC_WAVE && myType & NORMAL_WAVE, "normal and numeric"
+	print myType & TEXT_WAVE && myType & FREE_WAVE, "free and text"
+End

procedures/unit-testing-constants.ipf
@@ -29,8 +29,7 @@ Constant REQUIRE_MODE   = 0x07 // == OUTPUT_MESSAGE | INCREASE_ERROR | ABORT_FUN
 
 /// @addtogroup testWaveFlags
 ///@{
-Constant TEXT_WAVE    = 2
-Constant NUMERIC_WAVE = 1
+Constant NULL_WAVE       = 0x00
 
 Constant COMPLEX_WAVE    = 0x01
 Constant FLOAT_WAVE      = 0x02
@@ -40,8 +39,14 @@ Constant INT16_WAVE      = 0x10
 Constant INT32_WAVE      = 0x20
 Constant INT64_WAVE      = 0x80
 Constant UNSIGNED_WAVE   = 0x40
-Constant DATAFOLDER_WAVE = 0x100
-Constant WAVE_WAVE       = 0x4000
+
+Constant NUMERIC_WAVE    = 0x01
+Constant TEXT_WAVE       = 0x02
+Constant DATAFOLDER_WAVE = 0x04
+Constant WAVE_WAVE       = 0x08
+
+Constant NORMAL_WAVE     = 0x10
+Constant FREE_WAVE       = 0x20
 ///@}
 
 /// @addtogroup equalWaveFlags

commit d91f78c2e1a1ada464264a160f190bc212adf9d1

Null Wave comparison
NULL_WAVE could not be as input to CHECK_WAVE() since binary comparison of
`(type & NULL_WAVE) == NULL_WAVE` will always yield 1.

bug introduced in 06b2e062:

To ensure correct behaviour the following two test cases were used:

```igor

Function MyTest()
	Make/FREE wv
	CHECK_WAVE(wv, NULL_WAVE)
End

Function MyTest2()
	WAVE/Z wv
	REQUIRE_WAVE(wv, NULL_WAVE)
End
```

procedures/unit-testing-comparators.ipf
@@ -489,7 +489,12 @@ static Function TEST_WAVE_WRAPPER(wv, majorType, flags, [minorType])
 	if(type2 > 0 && type2 <= 2)
 		type = type | 2^(type2 + 3)
 	endif
-	result = (type & majorType) == majorType
+
+	if(majorType == NULL_WAVE)
+		result = type == majorType
+	else
+		result = (type & majorType) == majorType
+	endif
 
 	sprintf str, "Assumption that the wave's main type is %d", majorType
 	SetTestStatusAndDebug(str, result)