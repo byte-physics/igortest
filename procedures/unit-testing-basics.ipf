byte-physics/igortest

commit fb8ef0ca02fa0a87e9a72bc5796b3a13333e4ce2

rework documentation
* add introduction
* add guided tour
* rework examples section
* add code linkings
* only document relevant code

A proper introduction for a general audience is added. The style is
oriented at an understanding for a 16year old.
introduce the utf with a simple example on a guided tour.

* self-explainatory changes for the build environment

procedures/unit-testing-basics.ipf
@@ -517,7 +517,7 @@ End
 
 ///@endcond // HIDDEN_SYMBOL
 
-///@addtogroup TestRunnerAndHelper
+///@addtogroup Helpers
 ///@{
 
 /// Turns debug output on
@@ -1098,18 +1098,69 @@ End
 
 ///@endcond // HIDDEN_SYMBOL
 
-///@addtogroup TestRunnerAndHelper
-///@{
-
-/// Main function to execute one or more test suites.
-/// @param   procWinList   semicolon (";") separated list of procedure files (must not include Independent Module specifications)
-/// @param   name           (optional) descriptive name for the executed test suites
-/// @param   testCase       (optional) function name, resembling one test case, which should be executed only for each test suite
-/// @param   enableJU       (optional) enables JUNIT xml output when set to 1
-/// @param   enableTAP      (optional) enables Test Anything Protocol (TAP) output when set to 1
-/// @param   enableRegExp   (optional) enables parsing of regular expressions within procWinList when set to 1. disabled on default.
-/// @param   allowDebug     (optional) when set != 0 then the Debugger does not get disabled while running the tests
-/// @param   keepDataFolder (optional) when set != 0 then the temporary Data Folder where the Test Case is executed in is not removed after the Test Case finishes
+/// @brief Main function to execute test suites with the unit testing framework.
+///
+/// @verbatim embed:rst:leading-slashes
+///     .. code-block:: igor
+///        :caption: usage example
+///
+///        RunTest("proc0;proc1", name="myTest")
+///
+///     This command will run the test suites `proc0` and `proc1` in a test named `myTest`.
+/// @endverbatim
+///
+/// @param   procWinList    A list of procedure files that should be handled as test suites.
+///                         @n The list should be given semicolon (";") separated.
+///                         @n The procedure name must not include Independent Module specifications.
+///                         @n This parameter can be treated as a regular expression with enableRegExp
+///                         
+/// @param   name           (optional) default "Unnamed" @n
+///                         descriptive name for the executed test suites. This can be
+///                         used to group multiple test suites into a single test run.
+///                         
+/// @param   testCase       (optional) default ".*" (all test cases in the list of test suites) @n
+///                         function names, resembling test cases, which should be
+///                         executed in the given list of test suites (procWinList).
+///                         @n The list should be given semicolon (";") separated.
+///                         @n This parameter can be treated as a regular expression with enableRegExp
+///                         
+/// @param   enableJU       (optional) default disabled, enabled when set to 1: @n
+///                         A JUNIT compatible XML file is written at the end of the Test Run.
+///                         It allows the combination of this framework with continuous integration
+///                         servers like Atlassian Bamboo.
+///                         
+/// @param   enableTAP      (optional) default disabled, enabled when set to 1: @n
+///                         A TAP compatible file is written at the end of the test run.
+///                         @verbatim embed:rst:leading-slashes
+///                             `Test Anything Protocol (TAP) <https://testanything.org>`__
+///                             `standard 13 <https://testanything.org/tap-version-13-specification.html>`__ 
+///                         @endverbatim
+///
+/// @param   enableRegExp   (optional) default disabled, enabled when set to 1: @n
+///                         The input for test suites (procWinList) and test cases (testCase) is
+///                         treated as a regular expression.
+///                         @verbatim embed:rst:leading-slashes
+///                             .. code-block:: igor
+///                                :caption: Example
+///
+///                                RunTest("example[1-3]-plain\\.ipf", enableRegExp=1)
+///
+///                             This command will run all test cases in the following test suites:
+///
+///                             * :ref:`example1-plain.ipf<example1>`
+///                             * :ref:`example2-plain.ipf<example2>`
+///                             * :ref:`example3-plain.ipf<example3>`
+///                         @endverbatim
+///
+/// @param   allowDebug     (optional) default disabled, enabled when set to 1: @n
+///                         The Igor debugger will be left in its current state when running the
+///                         tests.
+///                         
+/// @param   keepDataFolder (optional) default disabled, enabled when set to 1: @n
+///                         The temporary data folder wherein each test case is executed is not
+///                         removed at the end of the test case. This allows to review the
+///                         produced data.
+///                         
 /// @return                 total number of errors
 Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp, allowDebug, keepDataFolder])
 	string procWinList, name, testCase
@@ -1276,5 +1327,3 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 
 	return global_error_count
 End
-
-///@}

docu/.gitignore
@@ -2,9 +2,9 @@ doxygen/doxygen.log
 doxygen/html/*
 doxygen/xml/*
 
-sphinx/*
-!sphinx/Makefile
-!sphinx/source/conf.py
-!sphinx/source/examples.py
-!sphinx/source/index.py
+sphinx/build
+sphinx/*.log
+sphinx/source/breathe
+sphinx/source/xml
+
 manual.pdf

docu/Makefile
@@ -17,6 +17,9 @@ docu-sphinx: build-docker-sphinx
 autobuild: build-docker-sphinx
 	@make -C docker/sphinx docker-autobuild
 
+debug: build-docker-sphinx
+	@make -C docker/sphinx sphinx-debug
+
 debian:
 	@make -C docker/debian
 

docu/docker/sphinx/Dockerfile
@@ -4,22 +4,24 @@ MAINTAINER Matthias J. Kastner matthias.kastner@byte-physics.de
 USER root
 RUN DEBIAN_FRONTEND=noninteractive \
 	apt-get update && \
+	apt-get upgrade --assume-yes && \
 	apt-get install --assume-yes --no-install-recommends \
-		doxygen \
+		doxygen=1.8.13* \
 		gawk \
 		git \
-        latexmk \
+		latexmk \
 		make \
 		python \
 		python-pip \
 		python-setuptools \
 		python-wheel \
-        texlive-fonts-recommended \
-        texlive-latex-extra \
-        texlive-latex-recommended && \
+		rsync \
+		texlive-fonts-recommended \
+		texlive-latex-extra \
+		texlive-latex-recommended && \
 	apt-get clean
 RUN pip install \
-	'breathe==4.9.1' \
+	'breathe==4.10.0' \
 	'sphinx==1.7.5' \
 	sphinx-autobuild \
 	sphinxcontrib-fulltoc

docu/docker/sphinx/Makefile
@@ -18,7 +18,7 @@ shell: build-docker
 
 # starts sphix autobuild for dev purposes
 docker-autobuild: build-docker
-	docker run --rm -it ${LINKDIR} ${LATEST} /bin/bash -c \
+	docker run --rm -it -p 8000:8000 ${LINKDIR} ${LATEST} /bin/bash -c \
 		'make -C "${DOCKER_TOP_LEVEL}/docu/sphinx" autobuild'
 
 # echo basic requirements for current container.
@@ -34,6 +34,9 @@ requirements: build-docker
 sphinx: requirements
 	docker run -it --rm ${LINKDIR} ${LATEST} /bin/bash /var/igor-unit-testing-framework/docu/scripts/docu-sphinx.sh
 
+sphinx-debug: requirements
+	docker run -it --rm ${LINKDIR} ${LATEST} /bin/bash -c \
+		'make -C "${DOCKER_TOP_LEVEL}/docu/sphinx" debug'
 links:
 	@stat "${LOCAL_TOP_LEVEL}/docu/sphinx/build/html/index.html"
 	@stat "${LOCAL_TOP_LEVEL}/docu/manual.pdf"

docu/examples/Example6/example6-automatic-invocation.ipf
@@ -4,17 +4,9 @@
 
 #include "unit-testing"
 
-// Shows automatic execution of Test Runs
-// Command: Call "autorun-test-xxx.bat" from the helper folder
-// Details:
-// the autorun command script executes Test Runs for all pxp experiment files
-// in the current folder open a command line in the example6 folder execute
-// "..\..\..\helper\autorun-test-IP7-64bit.bat" for e.g. Igor Pro 7 64 bit.
-// Igor Pro is expected to be in the default installation folder.  After the
-// run a log file in the example6 folder with the history can be found.
-
 static Function CheckTrigonometricFunctions()
-	CHECK_EQUAL_VAR(sin(0.0),0.0)
-	CHECK_EQUAL_VAR(cos(0.0),1.0)
-	CHECK_EQUAL_VAR(tan(0.0),0.0)
+
+	CHECK_EQUAL_VAR(sin(0.0), 0.0)
+	CHECK_EQUAL_VAR(cos(0.0), 1.0)
+	CHECK_EQUAL_VAR(tan(0.0), 0.0)
 End

docu/examples/Example6/example6-runner.ipf
@@ -4,5 +4,6 @@
 #include "unit-testing"
 
 Function run()
+
 	RunTest("example6-automatic-invocation.ipf")
 End

docu/examples/example1-plain.ipf
@@ -3,18 +3,12 @@
 
 #include "unit-testing"
 
-// Execute the test suite, same named as this procedure file
-// with RunTest("example1-plain.ipf")
+Function TestAbs()
 
-Function TestModulo()
-
-	CHECK_EQUAL_VAR(abs(1.5),1.5)
-	CHECK_EQUAL_VAR(abs(-1.5),1.5)
-	CHECK_EQUAL_VAR(abs(NaN),NaN)
-	// remember that NaN is not equal to NaN
-	// this check will generate a warning message but due
-	// to the usage of WARN instead of CHECK not increment the error count
+	CHECK_EQUAL_VAR(abs(1.5), 1.5)
+	CHECK_EQUAL_VAR(abs(-1.5), 1.5)
+	CHECK_EQUAL_VAR(abs(NaN), NaN)
 	WARN(abs(NaN) == NaN)
-	CHECK_EQUAL_VAR(abs(INF),INF)
-	CHECK_EQUAL_VAR(abs(-INF),INF)
+	CHECK_EQUAL_VAR(abs(INF), INF)
+	CHECK_EQUAL_VAR(abs(-INF), INF)
 End

commit a2717d0aa66ae7bb1e830c72e96c101627b73d44

Fix ws errors
Fix white space errors in comment block section of `RunTest()`,
introduced in fb8ef0ca.

procedures/unit-testing-basics.ipf
@@ -1113,27 +1113,27 @@ End
 ///                         @n The list should be given semicolon (";") separated.
 ///                         @n The procedure name must not include Independent Module specifications.
 ///                         @n This parameter can be given as a regular expression with enableRegExp set to 1.
-///                         
+///
 /// @param   name           (optional) default "Unnamed" @n
 ///                         descriptive name for the executed test suites. This can be
 ///                         used to group multiple test suites into a single test run.
-///                         
+///
 /// @param   testCase       (optional) default ".*" (all test cases in the list of test suites) @n
 ///                         function names, resembling test cases, which should be
 ///                         executed in the given list of test suites (procWinList).
 ///                         @n The list should be given semicolon (";") separated.
 ///                         @n This parameter can be treated as a regular expression with enableRegExp set to 1.
-///                         
+///
 /// @param   enableJU       (optional) default disabled, enabled when set to 1: @n
 ///                         A JUNIT compatible XML file is written at the end of the Test Run.
 ///                         It allows the combination of this framework with continuous integration
 ///                         servers like Atlassian Bamboo.
-///                         
+///
 /// @param   enableTAP      (optional) default disabled, enabled when set to 1: @n
 ///                         A TAP compatible file is written at the end of the test run.
 ///                         @verbatim embed:rst:leading-slashes
 ///                             `Test Anything Protocol (TAP) <https://testanything.org>`__
-///                             `standard 13 <https://testanything.org/tap-version-13-specification.html>`__ 
+///                             `standard 13 <https://testanything.org/tap-version-13-specification.html>`__
 ///                         @endverbatim
 ///
 /// @param   enableRegExp   (optional) default disabled, enabled when set to 1: @n
@@ -1155,12 +1155,12 @@ End
 /// @param   allowDebug     (optional) default disabled, enabled when set to 1: @n
 ///                         The Igor debugger will be left in its current state when running the
 ///                         tests.
-///                         
+///
 /// @param   keepDataFolder (optional) default disabled, enabled when set to 1: @n
 ///                         The temporary data folder wherein each test case is executed is not
 ///                         removed at the end of the test case. This allows to review the
 ///                         produced data.
-///                         
+///
 /// @return                 total number of errors
 Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp, allowDebug, keepDataFolder])
 	string procWinList, name, testCase