byte-physics/igortest

commit 713635a81a5bfc5b14ffdd944c7aa228e9e120d1

Nificy properties output in JUNIT XML file
Adding a carriage return after every list element makese the output
appear much nicer then postprocessed with [1].

[1]: https://gitlab.com/inorton/junit2html

procedures/unit-testing-junit.ipf
@@ -314,6 +314,20 @@ Function JU_TestBegin(s)
 	s.testSuiteOut = ""
 End
 
+/// Add a EOL ('\r') after every element of a `;` separated list.
+/// Intended for better readability.
+static Function/S JU_NicifyList(list)
+	string list
+
+	list = RemoveEnding(list, ";")
+
+	if(strlen(list) == 0)
+		return list
+	endif
+
+	return ReplaceString(";", list, ";\r")
+End
+
 /// Prepares JUNIT Test Suite structure for a new Test Suite
 Function JU_TestSuiteBegin(s, name, procWin)
 	STRUCT JU_Props& s
@@ -340,10 +354,10 @@ Function JU_TestSuiteBegin(s, name, procWin)
 	s.juTSProp.propNameList = ""
 	s.juTSProp.propValueList = ""
 	s.testCaseListOut = ""
-	JU_AddTSProp(s.juTSProp, "IgorInfo", IgorInfo(0))
+	JU_AddTSProp(s.juTSProp, "IgorInfo", JU_NicifyList(IgorInfo(0)))
 	JU_AddTSProp(s.juTSProp, "UTFversion", GetVersion())
 	JU_AddTSProp(s.juTSProp, "Experiment", IgorInfo(1))
-	JU_AddTSProp(s.juTSProp, "System", IgorInfo(3))
+	JU_AddTSProp(s.juTSProp, "System", JU_NicifyList(IgorInfo(3)))
 #if (IgorVersion() >= 7.00)
 	strswitch(IgorInfo(2))
 		case "Windows":

commit e98d32355f90b2b3275664c711845bbe7c61a383

JU_NicifyList: Fix documentation build
Broken since 713635a8 (Nificy properties output in JUNIT XML file, 2019-03-13).

procedures/unit-testing-junit.ipf
@@ -314,7 +314,7 @@ Function JU_TestBegin(s)
 	s.testSuiteOut = ""
 End
 
-/// Add a EOL ('\r') after every element of a `;` separated list.
+/// Add a EOL (`\r`) after every element of a `;` separated list.
 /// Intended for better readability.
 static Function/S JU_NicifyList(list)
 	string list