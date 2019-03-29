byte-physics/igortest

commit 21b80bd2a89fde0cca90c31717ba92ef860a9171

RunTest: Disallow enabling both TAP and JUNIT output
We never supported enabling both so let's be honest and complain.

procedures/unit-testing-basics.ipf
@@ -1129,12 +1129,16 @@ End
 ///                         It allows the combination of this framework with continuous integration
 ///                         servers like Atlassian Bamboo/GitLab/etc.
 ///
+///                         Can not be combined with enableTAP.
+///
 /// @param   enableTAP      (optional) default disabled, enabled when set to 1: @n
 ///                         A TAP compatible file is written at the end of the test run.
 ///                         @verbatim embed:rst:leading-slashes
 ///                             `Test Anything Protocol (TAP) <https://testanything.org>`__
 ///                             `standard 13 <https://testanything.org/tap-version-13-specification.html>`__
 ///                         @endverbatim
+///
+///                         Can not be combined with enableJU.
 ///
 /// @param   enableRegExp   (optional) default disabled, enabled when set to 1: @n
 ///                         The input for test suites (procWinList) and test cases (testCase) is
@@ -1190,9 +1194,16 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 	enableTAP = ParamIsDefault(enableTAP) ? 0 : !!enableTAP
 	allowDebug = ParamIsDefault(allowDebug) ? 0 : !!allowDebug
 	keepDataFolder = ParamIsDefault(keepDataFolder) ? 0 : !!keepDataFolder
+
+	if(enableTAP && juProps.enableJU)
+		printf "Error: enableTAP and enableJU can not be both true.\r"
+		return NaN
+	endif
+
 	if(ParamIsDefault(name))
 		name = "Unnamed"
 	endif
+
 	if(ParamIsDefault(testCase))
 		testCase = ".*"
 		enableRegExpTC = 1

commit 29c658e5175c4fd24634323f2bf39f5393298b0f

RunTest: Fix documentation
We can't have paragraphs in @param.

Broken since 21b80bd2 (RunTest: Disallow enabling both TAP and JUNIT
output, 2019-03-13).

procedures/unit-testing-basics.ipf
@@ -1257,7 +1257,6 @@ End
 ///                         A JUNIT compatible XML file is written at the end of the Test Run.
 ///                         It allows the combination of this framework with continuous integration
 ///                         servers like Atlassian Bamboo/GitLab/etc.
-///
 ///                         Can not be combined with enableTAP.
 ///
 /// @param   enableTAP      (optional) default disabled, enabled when set to 1: @n
@@ -1266,7 +1265,6 @@ End
 ///                             `Test Anything Protocol (TAP) <https://testanything.org>`__
 ///                             `standard 13 <https://testanything.org/tap-version-13-specification.html>`__
 ///                         @endverbatim
-///
 ///                         Can not be combined with enableJU.
 ///
 /// @param   enableRegExp   (optional) default disabled, enabled when set to 1: @n