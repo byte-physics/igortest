ad1c93d4f1c8942da033fdf59fb3b641481e8d14
Thomas Braun <thomas.braun@byte-physics.de>
2019-07-17 11:25:06 +0200

    Documentation: Update matomo snippet to the latest version

--- docu/sphinx/source/_static/js/matomo.js
+++ docu/sphinx/source/_static/js/matomo.js
@@ -1,18 +1,18 @@
 <!-- Matomo -->
 <script type="text/javascript">
-  var _paq = _paq || [];
+  var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setCookieDomain", "*.byte-physics.de"]);
+  _paq.push(["setDomains", "*.byte-physics.de"]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
     var u="https://www.byte-physics.de/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
-    _paq.push(['setCookieDomain', '*.byte-physics.de']);
-    _paq.push(['setDomains', '*.byte-physics.de']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="https://www.byte-physics.de/piwik/piwik.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="https://www.byte-physics.de/piwik/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Matomo Code -->
