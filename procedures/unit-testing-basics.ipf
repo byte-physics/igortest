byte-physics/igortest

commit a3ab7ca8a863c37d2a4b94d4e9c7cc0db4582655

Bugfix TAP now also outputs skipped testcases
If a test case was skipped, it would not appear in TAP output.
Now it gets numbered and marked as OK.

procedures/unit-testing-basics.ipf
@@ -2630,9 +2630,9 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 				s.tcSuffix = ""
 			endif
 
-			if(!s.tap_skipCase || reentry)
+			do
 
-				do
+				if(!s.tap_skipCase || reentry)
 
 					if(!reentry)
 
@@ -2708,13 +2708,16 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 						break
 					endif
 
-					TAP_WriteCaseIfReq(s.tap_caseCount, s.tap_skipCase)
-					s.tap_caseCount += 1
+				else
+					TAP_SetDirectiveAndDescription(s.fullFuncName)
+				endif
+				
+				TAP_WriteCaseIfReq(s.tap_caseCount, s.tap_skipCase)
+				s.tap_caseCount += 1
 
-					s.dgenIndex += 1
-				while(s.mdMode && s.dgenIndex < s.dgenSize)
+				s.dgenIndex += 1
 
-			endif
+			while(s.mdMode && s.dgenIndex < s.dgenSize)
 
 		endfor
 

procedures/unit-testing-tap.ipf
@@ -241,11 +241,11 @@ static Function TAP_WriteCase(case_cnt, skipcase)
 
 	if(skipcase)
 		str_ok = "ok"
+		tap_diagnostic = ""
 	else
 		str_ok = SelectString(tap_caseErr == 0, "not ok", "ok")
+		tap_diagnostic = TAP_ValidDiagnostic(tap_diagnostic)
 	endif
-
-	tap_diagnostic = TAP_ValidDiagnostic(tap_diagnostic)
 
 	// Write Out Test Case Result, TAP counts starting with 1
 	sprintf str_out, "%s %d %s %s" + TAP_LINEEND_STR, str_ok, case_cnt, tap_description, tap_directive

commit 2c82b4bae728f3bfc04ee06d6a45d2cf86a6d247

Fix for wrong TC counting in TAP when SKIP was used with MD generators
- When using a #TAPDirective: SKIP on Multi Data Tests the global TAP count
  included the MD data tests but the skipped MD test was only counted once.
- Moved the TAP skip decision to the execution block for the TC function
  and TC hooks
- Removed unneeded call to TAP_GetNotes after each skipped TC

Bug was introduced in a3ab7ca

procedures/unit-testing-basics.ipf
@@ -2655,40 +2655,42 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 
 			do
 
-				if(!s.tap_skipCase || reentry)
+				if(!reentry)
 
-					if(!reentry)
-
-						FUNCREF TEST_CASE_PROTO TestCaseFunc = $s.fullFuncName
-						if(UTF_FuncRefIsAssigned(FuncRefInfo(TestCaseFunc)))
-							s.mdMode = 0
-						else
-							s.mdMode = 1
-							s.dgenFuncName = UTF_Utils#GetFunctionTagValue(s.fullFuncName, UTF_FTAG_TD_GENERATOR, var)
-							s.dgenFuncName = GetFullFunctionName(var, s.dgenFuncName, s.procWin)
-							FUNCREF TEST_CASE_PROTO_DGEN DataGenFunc = $s.dgenFuncName
-							WAVE wGenerator = DataGenFunc()
-							s.dgenSize = DimSize(wGenerator, 0)
-							s.tcSuffix = ":" + GetDimLabel(wGenerator, 0, s.dgenIndex)
-							if(strlen(s.tcSuffix) == 1)
-								s.tcSuffix = ":" + num2str(s.dgenIndex)
-							endif
-						endif
-						ExecuteHooks(TEST_CASE_BEGIN_CONST, s.procHooks, s.juProps, s.fullFuncName + s.tcSuffix, s.procWin)
+					FUNCREF TEST_CASE_PROTO TestCaseFunc = $s.fullFuncName
+					if(UTF_FuncRefIsAssigned(FuncRefInfo(TestCaseFunc)))
+						s.mdMode = 0
 					else
-
-						DFREF dfSave = $PKG_FOLDER_SAVE
-						RestoreState(dfSave, s)
-						// restore all loop counters and end loop locals
-						i = s.i
-						j = s.j
-						numItemsPW = ItemsInList(s.procWinList)
-						numItemsFFN = ItemsInList(s.testCaseList)
-						// restore state done
-						DFREF dfSave = $""
-						ClearReentrytoUTF()
-
+						s.mdMode = 1
+						s.dgenFuncName = UTF_Utils#GetFunctionTagValue(s.fullFuncName, UTF_FTAG_TD_GENERATOR, var)
+						s.dgenFuncName = GetFullFunctionName(var, s.dgenFuncName, s.procWin)
+						FUNCREF TEST_CASE_PROTO_DGEN DataGenFunc = $s.dgenFuncName
+						WAVE wGenerator = DataGenFunc()
+						s.dgenSize = DimSize(wGenerator, 0)
+						s.tcSuffix = ":" + GetDimLabel(wGenerator, 0, s.dgenIndex)
+						if(strlen(s.tcSuffix) == 1)
+							s.tcSuffix = ":" + num2str(s.dgenIndex)
+						endif
 					endif
+					if(!s.tap_skipCase)
+						ExecuteHooks(TEST_CASE_BEGIN_CONST, s.procHooks, s.juProps, s.fullFuncName + s.tcSuffix, s.procWin)
+					endif
+				else
+
+					DFREF dfSave = $PKG_FOLDER_SAVE
+					RestoreState(dfSave, s)
+					// restore all loop counters and end loop locals
+					i = s.i
+					j = s.j
+					numItemsPW = ItemsInList(s.procWinList)
+					numItemsFFN = ItemsInList(s.testCaseList)
+					// restore state done
+					DFREF dfSave = $""
+					ClearReentrytoUTF()
+
+				endif
+
+				if(!s.tap_skipCase)
 
 					try
 						ClearRTError()
@@ -2714,25 +2716,25 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 						endif
 					endtry
 
-					reentry = 0
+				endif
 
-					if(IsBckgRegistered())
-						// save state
-						NewDataFolder $PKG_FOLDER_SAVE
-						DFREF dfSave = $PKG_FOLDER_SAVE
-						SaveState(dfSave, s)
+				reentry = 0
 
-						return RUNTEST_RET_BCKG
-					endif
+				if(IsBckgRegistered())
+					// save state
+					NewDataFolder $PKG_FOLDER_SAVE
+					DFREF dfSave = $PKG_FOLDER_SAVE
+					SaveState(dfSave, s)
 
+					return RUNTEST_RET_BCKG
+				endif
+
+				if(!s.tap_skipCase)
 					ExecuteHooks(TEST_CASE_END_CONST, s.procHooks, s.juProps, s.fullFuncName + s.tcSuffix, s.procWin, param = s.keepDataFolder)
+				endif
 
-					if(shouldDoAbort())
-						break
-					endif
-
-				else
-					TAP_SetDirectiveAndDescription(s.fullFuncName)
+				if(shouldDoAbort())
+					break
 				endif
 				
 				TAP_WriteCaseIfReq(s.tap_caseCount, s.tap_skipCase)