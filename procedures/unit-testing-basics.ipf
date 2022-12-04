byte-physics/igortest

commit 0c33b519103f822f2bceef5bf55217bb9a94c661

Enable fixed log file names
RunTest got a new optional argument fixLogName for which the output log
file get a predictable name. The fixed log file name is IUTF_Test.log.
If this flag is not set the naming will be unchanged and contains the
current timestamp.

Close #38.

procedures/unit-testing-basics.ipf
@@ -63,6 +63,8 @@ Constant IUTF_DEBUG_NVAR_SVAR_WAVE = 0x04
 Constant IUTF_DEBUG_FAILED_ASSERTION = 0x08
 /// @}
 
+static StrConstant FIXED_LOG_FILENAME = "IUTF_Test"
+
 StrConstant IUTF_TRACE_REENTRY_KEYWORD = " *** REENTRY ***"
 
 static Constant TEST_CASE_TYPE = 0x01
@@ -3404,12 +3406,18 @@ End
 ///                         HTMLCREATION:boolean When set to zero, no htm result files are created at the end of the run
 ///                         REGEXP:boolean When set, traceWinList is interpreted as regular expression
 ///
+/// @param   fixLogName     (optional) default 0 disabled, enabled when set to 1: @n
+///	                        If enabled the output files that will be generated after an autorun will have predictable names like
+///                         "IUTF_Test.log". If disabled the file names will always contain the name of the procedure file and a
+///                         timestamp.
+///
 /// @return                 total number of errors
-Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp, allowDebug, debugMode, keepDataFolder, traceWinList, traceOptions])
+Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp, allowDebug, debugMode, keepDataFolder, traceWinList, traceOptions, fixLogName])
 	string procWinList, name, testCase
 	variable enableJU, enableTAP, enableRegExp
 	variable allowDebug, debugMode, keepDataFolder
 	string traceWinList, traceOptions
+	variable fixLogName
 
 	// All variables that are needed to keep the local function state are wrapped in s
 	// new var/str must be added to strRunTest and added in SaveState/RestoreState functions
@@ -3429,6 +3437,8 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 	variable var, err
 	string msg, errMsg
 
+	fixLogName = ParamIsDefault(fixLogName) ? 0 : !!fixLogName
+
 	reentry = IsBckgRegistered()
 	ResetBckgRegistered()
 	if(reentry)
@@ -3447,6 +3457,8 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 
 	else
 		// no early return/abort above this point
+		DFREF dfr = GetPackageFolder()
+		string/G dfr:baseFilenameOverwrite = SelectString(fixLogName, FIXED_LOG_FILENAME, "")
 		ClearTestSetupWaves()
 		ClearBaseFilename()
 		CreateHistoryLog()

procedures/unit-testing-autorun.ipf
@@ -100,6 +100,7 @@ End
 Function/S GetBaseFilename()
 	dfref dfr = GetPackageFolder()
 	SVAR/Z/SDFR=dfr baseFilename
+	SVAR/Z/SDFR=dfr baseFilenameOverwrite
 
 	if(!SVAR_Exists(baseFilename))
 		string/G dfr:baseFilename = ""
@@ -109,7 +110,13 @@ Function/S GetBaseFilename()
 	if(strlen(baseFilename))
 		return baseFilename
 	endif
-	sprintf baseFilename, "%s_%s_%s", IgorInfo(1), Secs2Date(DateTime, -2), ReplaceString(":", Secs2Time(DateTime, 3), "-")
+
+	if(SVAR_Exists(baseFilenameOverwrite) && strlen(baseFilenameOverwrite))
+		baseFilename = baseFilenameOverwrite
+	else
+		sprintf baseFilename, "%s_%s_%s", IgorInfo(1), Secs2Date(DateTime, -2), ReplaceString(":", Secs2Time(DateTime, 3), "-")
+	endif
+
 	return baseFilename
 End
 

commit baf9e5a3149d6a5a559238b1ccdeebd50116eea4

RunTest: Flip argument for SelectString with fixLogName
Broken since 0c33b519 (Enable fixed log file names, 2022-10-30).

procedures/unit-testing-basics.ipf
@@ -3652,7 +3652,7 @@ Function RunTest(procWinList, [name, testCase, enableJU, enableTAP, enableRegExp
 	else
 		// no early return/abort above this point
 		DFREF dfr = GetPackageFolder()
-		string/G dfr:baseFilenameOverwrite = SelectString(fixLogName, FIXED_LOG_FILENAME, "")
+		string/G dfr:baseFilenameOverwrite = SelectString(fixLogName, "", FIXED_LOG_FILENAME)
 		ClearTestSetupWaves()
 		ClearBaseFilename()
 		CreateHistoryLog()